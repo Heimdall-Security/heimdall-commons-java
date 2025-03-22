67d515225b2d7f22b176a5122b74e5a31ad91248
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-03-22 13:45:05 -0600

    HAS-36 improve(utils): Update to the MetadataUtils

--- src/main/java/com/heimdallauth/server/utils/MetadataUtils.java
+++ src/main/java/com/heimdallauth/server/utils/MetadataUtils.java
@@ -2,6 +2,8 @@ package com.heimdallauth.server.utils;
 
 import java.time.Instant;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class MetadataUtils {
     private static final String CREATION_TIMESTAMP_KEY = "creationTimestamp";
@@ -11,5 +13,19 @@ public class MetadataUtils {
           HeimdallMetadata.builder().key(CREATION_TIMESTAMP_KEY).value(Instant.now().toString()).build(),
             HeimdallMetadata.builder().key(UPDATE_TIMESTAMP_KEY).value(Instant.now().toString()).build()
         );
+    }
+    public static List<HeimdallMetadata> createAndAddMetadataToExistingMetadata(List<HeimdallMetadata> metadata, String key, String value){
+        return Stream.concat(metadata.stream(), Stream.of(HeimdallMetadata.builder().key(key).value(value).build()))
+                     .collect(Collectors.toList());
+    }
+    public static List<HeimdallMetadata> updateTimestamp(List<HeimdallMetadata> metadata){
+        return metadata.stream()
+                .map(m -> {
+                    if(m.getKey().equals(UPDATE_TIMESTAMP_KEY)){
+                        return HeimdallMetadata.builder().key(m.getKey()).value(Instant.now().toString()).build();
+                    }
+                    return m;
+                })
+                .collect(Collectors.toList());
     }
 }
