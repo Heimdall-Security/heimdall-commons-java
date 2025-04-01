12bac6c0031003f555578eb22a3da79feaa7c317
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-04-01 02:39:14 -0700

    HAS-39 proto: Update to use records for immutable objects such as DTO

--- src/main/java/com/heimdallauth/server/dto/bifrost/CreateConfigurationSetDTO.java
+++ src/main/java/com/heimdallauth/server/dto/bifrost/CreateConfigurationSetDTO.java
@@ -1,19 +1,14 @@
 package com.heimdallauth.server.dto.bifrost;
 
 import com.heimdallauth.server.models.bifrost.SmtpProperties;
-import lombok.*;
 
 import java.util.List;
 import java.util.UUID;
 
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-public class CreateConfigurationSetDTO {
-    private String configurationSetName;
-    private String configurationSetDescription;
-    private List<UUID> emailSuppressionEntries;
-    private SmtpProperties smtpProperties;
+public record CreateConfigurationSetDTO(
+        String configurationSetName,
+        String configurationSetDescription,
+        List<UUID> suppressionEntryIds,
+        SmtpProperties smtpProperties
+) {
 }
