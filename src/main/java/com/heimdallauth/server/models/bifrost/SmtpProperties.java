ddf2f5caefbbae4d8c2cf87038df19792483ba88
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-04-05 17:49:29 -0700

    HAS-39 Update to the SMTP related properties

--- src/main/java/com/heimdallauth/server/models/bifrost/SmtpProperties.java
+++ src/main/java/com/heimdallauth/server/models/bifrost/SmtpProperties.java
@@ -1,15 +1,20 @@
 package com.heimdallauth.server.models.bifrost;
 
 import com.heimdallauth.server.constants.bifrost.SmtpAuthenticationMethod;
-import com.heimdallauth.server.constants.bifrost.SmtpEncryption;
+
+import java.util.Map;
 
 public record SmtpProperties(
         String serverAddress,
-        int port,
-        String username,
-        String password,
-        boolean useSSLTls,
-        SmtpEncryption smtpEncryption,
-        SmtpAuthenticationMethod authenticationMethod
+        int portNumber,
+        String loginUsername,
+        String loginPassword,
+        String fromEmailAddress,
+        String replyToEmailAddress,
+        SmtpAuthenticationMethod authenticationMethod,
+        int connectionLimit,
+        long maxIdleTime,
+        boolean enabled,
+        Map<String, String> messageHeaders
 ) {
 }
