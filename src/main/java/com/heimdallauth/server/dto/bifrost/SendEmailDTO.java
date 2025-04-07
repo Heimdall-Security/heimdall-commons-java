ca81e79f1e7b3db90797847b79c34c0d60e5a6cc
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-04-07 09:19:29 -0700

    HAS-31 improve(DTO): Updated the DTO to hold configurationSetId

--- src/main/java/com/heimdallauth/server/dto/bifrost/SendEmailDTO.java
+++ src/main/java/com/heimdallauth/server/dto/bifrost/SendEmailDTO.java
@@ -4,8 +4,10 @@ import com.heimdallauth.server.models.bifrost.EmailDestination;
 import com.heimdallauth.server.models.bifrost.EmailTemplateContent;
 
 import java.util.List;
+import java.util.UUID;
 
 public record SendEmailDTO(
+        UUID configurationSetId,
         EmailDestination destination,
         EmailTemplateContent content,
         List<String> replyToEmails
