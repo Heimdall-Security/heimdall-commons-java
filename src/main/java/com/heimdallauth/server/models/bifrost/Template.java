baf367e87987380125c1a04d69bc55a5c1144ef5
Mayank Soni <mayank.soni@mayanksoni.tech>
2025-04-08 17:09:21 -0700

    HAS-46 HAS-48 improve(Model): Updated the TemplateModel

--- src/main/java/com/heimdallauth/server/models/bifrost/Template.java
+++ src/main/java/com/heimdallauth/server/models/bifrost/Template.java
@@ -1,6 +1,7 @@
 package com.heimdallauth.server.models.bifrost;
 
 import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 public record Template(
@@ -8,6 +9,7 @@ public record Template(
         String templateDescription,
         UUID tenantId,
         EmailContent content,
+        List<MessageHeader> defaultEmailHeaders,
         Instant createdAt,
         Instant updatedAt
 ) {
